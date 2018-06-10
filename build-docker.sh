f40f53da7c3676bb70a2d909df6cf380f6808f5f
James Paine <painejs@hotmail.com>
2018-06-10 20:36:07 +0100

    Fix travis.yml syntax

--- build-docker.sh
+++
@@ -1,8 +0,0 @@
-REPO=painejs/pantry-for-good
-
-docker login -e $QUAYIO_EMAIL -u $QUAYIO_USERNAME -p $QUAYIO_PASSWORD
-
-docker build -t $REPO
-docker tag $REPO $REPO:latest
-docker tag $REPO $REPO:$TRAVIS_TAG
-docker push $REPO
